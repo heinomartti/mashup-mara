6ce11686011aa554a87be9b43cdd12bea2804055
mheinone <heinomartti@gmail.com>
2015-02-04 17:14:28 +0200

    Packaging data retrieval

--- program.js
+++ program.js
@@ -1,40 +1,81 @@
+"use strict";
 var http = require('http');
 var _ = require('lodash');
 var port = 80;
-var statusHtml = "<html><body>No data available</body></html>";
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end(statusHtml);
-}).listen(port, '127.0.0.1');
+ 
+var requestHandler = function (req, res) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        var response = dataProvider.getCombinedDataAsHTML() + dataProvider.getOtherStuffAsHtml() + dataProvider.getBooksAsHtml(); 
+        res.end(response);
+}
+ 
+var server = http.createServer(requestHandler);
+server.listen(port, '127.0.0.1');
 console.log('Server running in port ' + port);
 
+var dataProvider = dataProvider || {};
+dataProvider = (function() {
+        var booksHtml = "<html><body>Books not available</body></html>"; 
+        var otherHtml = "<html><body>Other stuff not available</body></html>"; 
+        var bookData = [];
+        var otherData = [];
+ 
+        var getBooks = function() {
+                var booksUrl = 'http://metadata.helmet-kirjasto.fi/search/author.json?query=Campbell';
+                http.get(booksUrl, function(res) {
+                        var body = "";
+                        res.on("data", function(chunk) {
+                                body += chunk;
+                        });
+ 
+                        res.on("end", function() {
+                                bookData = _.map(JSON.parse(body).records, function(d) {
+                                        return {
+                                                displayName: d.title,
+                                                year: d.year
+                                        };
+                                });
+                                console.log("Got list of books:", bookData);
+                               
+                                 booksHtml = "<html><body>";
+                                _.map(bookData, function(d) {
+                                         booksHtml += "<h1>" + d.displayName + "</h1>";
+                                         booksHtml += "<p>" + d.year + "</p>";
+                                });
+                                booksHtml += "</body></html>";
+                        });
+                }).on("error", function(e) {
+                        console.log("Error: ", e);
+                });
+        }
 
-var booksUrl = 'http://metadata.helmet-kirjasto.fi/search/author.json?query=Campbell';
+        var getBooksAsHtml = function() {
+            return booksHtml;
+        }    
 
-http.get(booksUrl, function(res) {
-	var body = "";
-	
-	res.on("data", function(chunk) {
-		body += chunk;
-	});
+        var getOtherStuff = function() {
+            // TODO
+        }
+    
+        var getOtherStuffAsHtml = function() {
+            return otherHtml;
+        }  
 
-	res.on("end", function() {
-		var bookList = _.map(JSON.parse(body).records, function(d) {
-			return {
-				displayName: d.title,
-				year: d.year
-			};
-		});
-		console.log("Got list of books:", bookList);
+        var getCombinedDataAsHTML = function (){
+            console.log(bookData);
+            var combinedHtml = bookData.toString() +  otherData.toString();
+            // TODO combine data with arrays, would be nice to have data that makes sense together
+            return combinedHtml;
+        }
+ 
+        return {
+            getBooks: getBooks,
+            getBooksAsHtml: getBooksAsHtml,
+            getOtherStuff: getOtherStuff,
+            getOtherStuffAsHtml: getOtherStuffAsHtml,
+            getCombinedDataAsHTML: getCombinedDataAsHTML
+        };
+}());
 
-		statusHtml = "<html><body>";
-		_.map(bookList, function(d) {
-			statusHtml += "<h1>" + d.displayName + "</h1>";
-			statusHtml += "<p>" + d.year + "</p>";
-		});
-	
-		statusHtml += "</body></html>";
-	});
-}).on("error", function(e) {
-	console.log("Error: ", e);
-});
+dataProvider.getBooks();
+dataProvider.getOtherStuff();
