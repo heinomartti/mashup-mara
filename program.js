f4ecaa645d49c9c09ef19862a460881656dd48b4
mheinone <heinomartti@gmail.com>
2015-02-04 18:17:21 +0200

    Combining two diffrent book queries

--- program.js
+++ program.js
@@ -5,7 +5,7 @@ var port = 80;
  
 var requestHandler = function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        var response = dataProvider.getCombinedDataAsHTML() + dataProvider.getOtherStuffAsHtml() + dataProvider.getBooksAsHtml(); 
+        var response = dataProvider.getCombinedDataAsHTML(); 
         res.end(response);
 }
  
@@ -15,14 +15,13 @@ console.log('Server running in port ' + port);
 
 var dataProvider = dataProvider || {};
 dataProvider = (function() {
-        var booksHtml = "<html><body>Books not available</body></html>"; 
-        var otherHtml = "<html><body>Other stuff not available</body></html>"; 
+        var combinedHtml = "<html><body>Data not available</body></html>"; 
         var bookData = [];
         var otherData = [];
  
         var getBooks = function() {
-                var booksUrl = 'http://metadata.helmet-kirjasto.fi/search/author.json?query=Campbell';
-                http.get(booksUrl, function(res) {
+                var url = 'http://metadata.helmet-kirjasto.fi/search/author.json?query=Campbell';
+                http.get(url, function(res) {
                         var body = "";
                         res.on("data", function(chunk) {
                                 body += chunk;
@@ -36,46 +35,58 @@ dataProvider = (function() {
                                         };
                                 });
                                 console.log("Got list of books:", bookData);
-                               
-                                 booksHtml = "<html><body>";
-                                _.map(bookData, function(d) {
-                                         booksHtml += "<h1>" + d.displayName + "</h1>";
-                                         booksHtml += "<p>" + d.year + "</p>";
-                                });
-                                booksHtml += "</body></html>";
                         });
                 }).on("error", function(e) {
                         console.log("Error: ", e);
                 });
         }
 
-        var getBooksAsHtml = function() {
-            return booksHtml;
-        }    
-
         var getOtherStuff = function() {
-            // TODO
+                var url = 'http://metadata.helmet-kirjasto.fi/search/author.json?query=Wayne';
+                http.get(url, function(res) {
+                        var body = "";
+                        res.on("data", function(chunk) {
+                                body += chunk;
+                        });
+ 
+                        res.on("end", function() {
+                                otherData = _.map(JSON.parse(body).records, function(d) {
+                                        return {
+                                                displayName: d.title,
+                                                author: d.author
+                                        };
+                                });
+                                console.log("Got list of other stuff:", otherData);
+                        });
+                }).on("error", function(e) {
+                        console.log("Error: ", e);
+                });
         }
-    
-        var getOtherStuffAsHtml = function() {
-            return otherHtml;
-        }  
 
         var getCombinedDataAsHTML = function (){
-            console.log(bookData);
-            var combinedHtml = bookData.toString() +  otherData.toString();
-            // TODO combine data with arrays, would be nice to have data that makes sense together
+            combinedHtml = "<html><body>";
+            _.map(otherData, function(d) {
+                combinedHtml += "<h1>" + d.displayName + "</h1>";
+                combinedHtml += "<p>" + d.author + "</p>";
+            });
+           _.map(bookData, function(d) {
+                combinedHtml += "<h1>" + d.displayName + "</h1>";
+                combinedHtml += "<p>" + d.year + "</p>";
+            });
+            combinedHtml += "</body></html>";
+
             return combinedHtml;
+        }
+
+        var initializeData = function (){
+            getBooks();
+            getOtherStuff();
         }
  
         return {
-            getBooks: getBooks,
-            getBooksAsHtml: getBooksAsHtml,
-            getOtherStuff: getOtherStuff,
-            getOtherStuffAsHtml: getOtherStuffAsHtml,
+            initializeData: initializeData,
             getCombinedDataAsHTML: getCombinedDataAsHTML
         };
 }());
 
-dataProvider.getBooks();
-dataProvider.getOtherStuff();
+dataProvider.initializeData();
